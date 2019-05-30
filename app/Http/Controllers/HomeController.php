59652c703b54bb782cca821ed9b1c69fd415e839
13480731740@qq.com <13480731740@qq.com>
2019-05-30 02:43:18 +0000

    采集页面信息完成

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('index');
     }
 
     /**
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $article = Article::paginate();
+        return view('home',compact('article'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">测试采集百家姓氏数据</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <ul class="list-group">
+                    @foreach($article as $v)
+                        <li class="list-group-item">{{ $v->title }}</li>
+                    @endforeach
+                    </ul>
                 </div>
+                {{ $article->links() }}
             </div>
         </div>
     </div>
