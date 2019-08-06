d450f881eec907a3626d2ea6995c12a06584872c
13480731740@qq.com <13480731740@qq.com>
2019-08-06 04:39:19 +0000

    取出文章和图片优化

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -19,7 +19,7 @@ class ArticleController extends Controller
 
     //提取图片
     public function gPic_Url($content){
-        $pattern="/<[img|IMG].*?src=[\'|\"](.*?(?:[\.gif|\.jpg]))[\'|\"].*?[\/]?>/";//正则
+        $pattern="/<[img|IMG].*?src=[\'|\"](.*?(?:[\.gif|\.jpg|\.png]))[\'|\"].*?[\/]?>/";//正则
         preg_match_all($pattern,$content,$match);//匹配图片
         return $match[1];//返回所有图片的路径
     }
